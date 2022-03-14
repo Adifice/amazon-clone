75d497a2eeaecd96e022e1f5f6baf331c2f4f537
Adifice <1047770652@qq.com>
2022-03-13 22:23:27 -0700

    day 2 Checkoutpage

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDpbGegQqZTWrJDq7ovKDrSt_w4ZZ57-ag",
+    authDomain: "clone-dd7e2.firebaseapp.com",
+    projectId: "clone-dd7e2",
+    storageBucket: "clone-dd7e2.appspot.com",
+    messagingSenderId: "130241376937",
+    appId: "1:130241376937:web:b6d99dd073ed18902d6fb8",
+    measurementId: "G-KE8RSRP04H"
+};
